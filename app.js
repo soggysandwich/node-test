f1043e5ed36bbef24aadde689adfa296d1c163fb
rich <rich@rich.com>
2022-08-19 10:18:20 +0100

    new app js file

--- app.js
+++ app.js
@@ -1,22 +1,2 @@
-//npm --version
 
-//local dependency only in this project
-//npm i <package name>
-
-//global dependency all projects 
-//npm install -g <packageName>
-//needs admin 
-
-//package.json - manifes file 
-//manual do it yourself make file 
-// npm init 
-//npm init -y (all default)
-
-const _ = require('lodash')
-
-const items = [1,[2,[3,[4]]]]
-
-const newItems = _.flattenDeep(items)
-
-console.log(newItems)
 
